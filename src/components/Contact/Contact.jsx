41f3478fcf6f7a3bf4a76bf4e14fa6ef8b47ee73
Pranav Tripathi <tripathipranav14@gmail.com>
2023-03-06 22:29:35 +0530

    add contact page

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import './Contact.css'
+
+const Contact = () => {
+    return (
+        <div className='contact-container'>
+            <div className='contact-heading-container'>
+                <h2 className='contact-heading'>Contact the Developers</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
